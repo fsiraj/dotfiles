4e620825542c6c4d16241ac7ff18cd05b3b59500
Farzad <farzad.siraj@memryx.com>
2024-11-27 12:51:05 -0500

    added first keymap for indent blanklines

--- .config/nvim/lua/plugins/indent_line.lua
+++ .config/nvim/lua/plugins/indent_line.lua
@@ -1,9 +1,10 @@
-return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+return { -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  config = function()
+    require('ibl').setup()
+    vim.keymap.set('n', '<leader>ti', ':IBLToggle<CR>', { desc = '[T]oggle [I]ndent blank lines' })
+  end,
 }
