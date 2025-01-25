5e72792ee3058b0e5256f6375751ce3dc6b26d13
Farzad <farzad.siraj@memryx.com>
2025-01-24 21:27:25 -0500

    added outline, treesitter textobjects, mappings, styling (and consolidated), quartered startup time, session management

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -6,13 +6,12 @@ return {
     {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
+        build = ':Copilot auth',
         event = 'InsertEnter',
-        config = function()
-            require('copilot').setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        },
     },
     {
         'olimorris/codecompanion.nvim',
@@ -51,6 +50,45 @@ return {
         end,
     },
 
+    -- Floating terminal
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        keys = {
+            { '<Bslash>', '<Cmd>ToggleTerm name=Terminal<CR>', mode = 'n', desc = 'Open Terminal' },
+        },
+        ---@module 'toggleterm'
+        ---@type ToggleTermConfig
+        opts = {
+            direction = 'float',
+            highlights = {
+                NormalFloat = { link = 'NormalFloat' },
+                FloatBorder = { link = 'FloatBorder' },
+            },
+            float_opts = {
+                width = math.floor(vim.o.columns * 0.6),
+                height = math.floor(vim.o.lines * 0.8),
+                border = 'single',
+                title_pos = 'center',
+            },
+        },
+        config = function(_, opts)
+            require('toggleterm').setup(opts)
+            vim.api.nvim_create_autocmd('TermOpen', {
+                pattern = 'term://*toggleterm#*',
+                callback = function()
+                    vim.keymap.set('n', 'q', '<Cmd>ToggleTerm<CR>', { buffer = true, desc = '[T]oggle [T]erm' })
+                    vim.keymap.set(
+                        { 't', 'n' },
+                        '<Esc><Esc>',
+                        '<Cmd>ToggleTerm<CR>',
+                        { buffer = true, desc = '[T]oggle [T]erm' }
+                    )
+                end,
+            })
+        end,
+    },
+
     -- Filesystem manager
     {
         'stevearc/oil.nvim',
@@ -70,7 +108,7 @@ return {
             keymaps = {
                 ['q'] = { 'actions.close' },
                 ['<C-h>'] = { 'actions.show_help' },
-                ['<C-_>'] = { 'actions.select', opts = { horizontal = true } },
+                ['<C-->'] = { 'actions.select', opts = { horizontal = true } },
                 ['<C-Bslash>'] = { 'actions.select', opts = { vertical = true } },
             },
             -- Optional dependencies
@@ -91,68 +129,26 @@ return {
         end,
     },
 
-    -- Find and replace
+    -- Symbol Outline
     {
-        'MagicDuck/grug-far.nvim',
+        'hedyhli/outline.nvim',
+        cmd = { 'Outline', 'OutlineOpen' },
         keys = {
-            {
-                '<Leader>fr',
-                ':GrugFar<CR>',
-                mode = { 'n', 'v' },
-                desc = '[F]ind [R]eplace',
-            },
+            { '<leader>fo', '<cmd>Outline<CR>', desc = '[F]ile [O]utline' },
         },
-        config = function() require('grug-far').setup({}) end,
+        opts = {
+            outline_window = { split_command = '40vsplit', winhl = 'Normal:NormalFloat' },
+            outline_items = { show_symbol_details = false },
+            preview_window = { winhl = 'NormalFloat:NormalFloat' },
+        },
     },
-
-    -- Add indentation guides even on blank lines
-    {
-        'lukas-reineke/indent-blankline.nvim',
-        main = 'ibl',
-        config = function()
-            require('ibl').setup({ exclude = { filetypes = { 'help', 'dashboard' } } })
-            vim.keymap.set('n', '<Leader>ti', ':IBLToggle<CR>', { desc = '[T]oggle [I]ndent blank lines' })
-        end,
-    },
-
-    -- Autopairs automatically adds matching parentheses, quotes, etc.
-    { 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
-
-    -- Detect tabstop and shiftwidth automatically
-    { 'tpope/vim-sleuth' },
 
     -- Highlight todo, notes, etc in comments
     {
         'folke/todo-comments.nvim',
-        event = 'VimEnter',
+        event = 'VeryLazy',
         dependencies = { 'nvim-lua/plenary.nvim' },
         opts = { signs = false },
-    },
-
-    -- Collection of various small independent plugins/modules
-    {
-        'echasnovski/mini.nvim',
-        config = function()
-            -- Better Around/Inside textobjects
-            require('mini.ai').setup({ n_lines = 500 })
-
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            require('mini.surround').setup()
-
-            -- Git tools, also used with codecompanion.nvim for single buffer diffs
-            require('mini.diff').setup({
-                view = { style = 'sign', signs = { add = '+', change = '~', delete = '-' }, priority = 5 },
-                mappings = {
-                    apply = '<Leader>ga',
-                    reset = '<Leader>gr',
-                    goto_prev = '<Leader>gp',
-                    goto_next = '<Leader>gn',
-                    goto_first = '<Leader>gg',
-                    goto_last = '<Leader>gG',
-                },
-            })
-            vim.keymap.set('n', '<Leader>tg', MiniDiff.toggle_overlay, { desc = '[T]oggle [G]it overlay' })
-        end,
     },
 
     -- Navigate between tmux and neovim seamlessly
@@ -171,35 +167,5 @@ return {
             { '<c-k>', '<cmd>TmuxNavigateUp<CR>' },
             { '<c-l>', '<cmd>TmuxNavigateRight<CR>' },
         },
-    },
-
-    -- Render markdown nicely
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        ft = { 'markdown', 'codecompanion' },
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    },
-
-    -- Preview markdown in browser
-    {
-        'iamcco/markdown-preview.nvim',
-        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-        ft = { 'markdown' },
-        build = function(plugin)
-            -- Install markdown preview, use npx if available.
-            if vim.fn.executable('npx') then
-                vim.cmd('!cd ' .. plugin.dir .. ' && cd app && npx --yes yarn install')
-            else
-                vim.cmd([[Lazy load markdown-preview.nvim]])
-                vim.fn['mkdp#util#install']()
-            end
-        end,
-        init = function()
-            if vim.fn.executable('npx') then vim.g.mkdp_filetypes = { 'markdown' } end
-            vim.keymap.set('n', '<Leader>tp', ':MarkdownPreviewToggle<CR>', { desc = '[T]oggle Markdown [P]review' })
-        end,
     },
 }
