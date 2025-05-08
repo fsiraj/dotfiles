0b5f9a1d649f8d6a88427e474b066b7ca210103b
fsiraj <fsiraj@umich.edu>
2025-05-07 23:14:22 -0400

    Fixed telecscope bug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,7 +114,7 @@ end, { silent = true, expr = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function() vim.highlight.on_yank({ timeout = 300 }) end,
+    callback = function() vim.hl.on_yank({ timeout = 300 }) end,
 })
 
 vim.api.nvim_create_autocmd('TermOpen', {
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -355,7 +355,6 @@ local M = {
     {
         'nvim-telescope/telescope.nvim',
         event = 'VeryLazy',
-        branch = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
             {
