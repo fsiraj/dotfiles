8a958a7ab8f0a2fb1caf996f037e6c49c426f609
Farzad <farzad.siraj@memryx.com>
2025-05-13 22:33:01 -0400

    Fixed diagnostic float showing when diagnostic disabled

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1096,11 +1096,13 @@ local M = {
             vim.api.nvim_create_autocmd({ 'CursorHold' }, {
                 pattern = '*',
                 callback = function()
-                    vim.diagnostic.open_float({
-                        scope = 'line',
-                        focusable = false,
-                        close_events = { 'CursorMoved', 'CursorMovedI' },
-                    })
+                    if vim.diagnostic.is_enabled() then
+                        vim.diagnostic.open_float({
+                            scope = 'line',
+                            focusable = false,
+                            close_events = { 'CursorMoved', 'CursorMovedI' },
+                        })
+                    end
                 end,
             })
 
