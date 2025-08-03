c5907c8d0e22d6e0e92ab88e18ef20c1b649b2e5
fsiraj <fsiraj@umich.edu>
2025-08-03 10:59:30 -0400

    Don't save hypr conf yet, figure out a better way

--- .config/hypr/scripts/laptop_lid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LID_STATE=$(cat /proc/acpi/button/lid/LID/state | grep -o 'open\|closed')
-if [ "$LID_STATE" = "closed" ]; then
-    hyprctl keyword monitor "eDP-1, disable"
-else
-    hyprctl keyword monitor "eDP-1, preferred, auto, auto"
-fi
